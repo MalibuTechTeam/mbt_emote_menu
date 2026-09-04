041278fa0e7c97486d5155f695bd1c6defeee90e
Manifest Bumper <41898282+github-actions[bot]@users.noreply.github.com>
2026-09-04 12:57:53 +0000

    chore: bump manifest version to v1.8.0

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,7 @@ game 'gta5'
 
 name 'mbt_emote_menu'
 author 'Malibù Tech Team'
-version      '1.7.0'
+version      '1.8.0'
 description 'Premium NUI emote menu for rpemotes-reborn by Malibu Tech'
 
 dependencies {
